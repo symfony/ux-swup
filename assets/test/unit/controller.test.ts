bd6e5a1319d0156c357055f1e59db1ead3ea9772
Hugo Alliaume <hugo@alliau.me>
2025-08-16 19:17:43 +0200

    Configure Vitest for unit and browser tests (use @puppeteer/browsers and webdriverio)

--- assets/test/unit/controller.test.ts
+++ assets/test/unit/controller.test.ts
@@ -10,8 +10,8 @@
 import { Application, Controller } from '@hotwired/stimulus';
 import { getByTestId, waitFor } from '@testing-library/dom';
 import { afterEach, describe, expect, it } from 'vitest';
-import { clearDOM, mountDOM } from '../../../../test/stimulus-helpers';
-import SwupController from '../src/controller';
+import { clearDOM, mountDOM } from '../../../../../test/stimulus-helpers';
+import SwupController from '../../src/controller';
 
 let actualSwupOptions: any = null;
 
